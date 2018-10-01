df3dfbde40f63de30b1faeb6b2a31c1862ccf060
rens gouman <99030922@mydavinci.nl>
2018-10-01 13:43:31 +0200

    autofocus add

--- webapp-for-questions/framework/view/webapp/login.php
+++ webapp-for-questions/framework/view/webapp/login.php
@@ -3,7 +3,7 @@
 ?>
 <h2>Login</h2>
 <form action="<?= URL ?>webapp/loginConfirm" method="POST">
-	<input type="text" placeholder="Enter Username" name="name" required >
+	<input type="text" autofocus placeholder="Enter Username" name="name" required >
 	<input type="password" placeholder="Enter Password" name="pwd" required >
 	<button type="submit" name="submit" required >login</button>
 </form>
--- webapp-for-questions/framework/view/webapp/signup.php
+++ webapp-for-questions/framework/view/webapp/signup.php
@@ -4,7 +4,7 @@
 <h2>Sign up</h2>
 <form action="<?= URL ?>webapp/signUpConfirm" method="POST">
 	<p>student name:</p>
-	<input type="text" placeholder="Enter Username" required name="name"><br>
+	<input autofocus type="text" placeholder="Enter Username" required name="name"><br>
 	
 	<p>Password:</p>
 	<input type="password" placeholder="Enter Password" required name="pwd"><br>
