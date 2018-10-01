d93c8eb00a47579ec3b2e28636af98d743b52230
Floris Lans <Floris.roderick@gmail.com>
2018-10-01 13:47:59 +0200

    login css

--- webapp-for-questions/framework/view/webapp/login.php
+++ webapp-for-questions/framework/view/webapp/login.php
@@ -1,9 +1,27 @@
 <?php
 
 ?>
-<h2>Login</h2>
+<div class="container">
+<h2>Login</h2> 
+
+<!-- name login bar -->
+
 <form action="<?= URL ?>webapp/loginConfirm" method="POST">
-	<input type="text" placeholder="Enter Username" name="name" required >
-	<input type="password" placeholder="Enter Password" name="pwd" required >
-	<button type="submit" name="submit" required >login</button>
+	<div class="group">
+	 <input type="text" name="name" required >
+	 <span class="highlight"></span>
+	 <span class="bar"></span>
+			<label>Naam</label>
+   </div>
+
+<!-- password login bar -->
+<div class="group">
+	<input type="password" name="pwd" required >
+	<span class="highlight"></span>
+	<span class="bar"></span>
+	<label>Wachtwoord</label>
+	<button id="login_button" type="submit" name="submit" required >login</button>
+</div>
 </form>
+<footer class="main_footer">footer template</footer>
+</div>
