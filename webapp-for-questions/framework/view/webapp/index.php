3cdfba09975b0b73f7c51384f487de0566331dfc
Bart Korver <99027534@mydavinci.nl>
2018-10-03 12:24:25 +0200

    index updated
    buttons fixed/removed

--- webapp-for-questions/framework/view/webapp/index.php
+++ webapp-for-questions/framework/view/webapp/index.php
@@ -1,19 +1,14 @@
 <?php 
 session_start();
-if (!$_SESSION['student_name'] == null) { ?>
-	<p>you are logged in as <?= $_SESSION['student_name'] ?></p>
-<?php
-}
 ?>
-<!--added questions_main id to differentiate this h2 -->
-<h2 id="questions_main">so many questions</h2>
+
 <table id="myTable">
 	<thead>
 		<tr>
-			<th colspan="1" onclick="sortTable(0)">student Name</th>
-			<th colspan="5" onclick="sortTable(1)">question</th>
-			<th colspan="2" onclick="sortTable(2)">time stamp</th>
-			<th colspan="1">status</th>
+			<th colspan="1" onclick="sortTable(0)">Student</th>
+			<th colspan="5" onclick="sortTable(1)">Vraag</th>
+			<th colspan="2" onclick="sortTable(2)">Tijd</th>
+			<th colspan="1">Status</th>
 
 			<?php 
 			if ($_SESSION['power_lvl'] == 1) { ?>
@@ -37,7 +32,7 @@ foreach ($questions as $question) {
 
 			<?php 
 			if ($_SESSION['power_lvl'] == 1) { ?>
-			<td class="center"><a href="<?= URL ?>webapp/editQuestionPage/<?= $question["question_id"] ?>">edit</a></td>
+			<td class="center"><a href="<?= URL ?>webapp/editQuestionPage/<?= $question["question_id"] ?>">Edit</a></td>
 			<?php 
 			}
 			?>
@@ -51,18 +46,7 @@ foreach ($questions as $question) {
 
 <?php 
 if (!$_SESSION['student_name'] == null) { ?>
-	<p><a href="<?= URL ?>webapp/createQuestionPage">new question</a></p>
+	<p><a href="<?= URL ?>webapp/createQuestionPage">Vraag stellen</a></p>
 <?php 
-}
-if ($_SESSION['student_name'] == null) { ?>
-	<h3><a href="<?= URL ?>webapp/loginPage">login</a></h3>
-<?php 
-}
-?>
-	<h3><a href="<?= URL ?>webapp/signUpPage">sign up</a></h3>
-<?php 
-if (!$_SESSION['student_name'] == null) { ?>
-	<h3><a href="<?= URL ?>webapp/logout">logout</a></h3>
-	<?php 
 }
 ?>
